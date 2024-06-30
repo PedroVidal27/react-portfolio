5fa8e2ced4b901ca58a76c8d951f7d6cbec51814
PedroVidal27 <pedrovidal2703@gmail.com>
2024-06-30 01:50:08 -0300

    :sparkles: setup page transition

--- src/app/hook/useDimensions.ts
+++ src/app/hook/useDimensions.ts
@@ -1,7 +1,52 @@
 "use client";
-import { useState, useEffect, SetStateAction } from "react";
+import { useState, useEffect } from "react";
+
+const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+};
 
 export default function useDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions(getWindowDimensions());
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isSmallDevice = () => {
+    const { width } = windowDimensions;
+    return width <= 675;
+  };
+
+  const isMediumDevice = () => {
+    const { width } = windowDimensions;
+    return width > 675 && width < 1450;
+  };
+
+  const isLargeDevice = () => {
+    const { width } = windowDimensions;
+    return width >= 1450;
+  };
+
+  const setSizeByScreen = (smallScreen: number, mediumScreen: number, largeScreen: number) => {
+    const { width } = windowDimensions;
+    if (width <= 675) {
+      return smallScreen;
+    }
+    if (width > 675 && width < 1450) {
+      return mediumScreen;
+    }
+    return largeScreen;
+  };
+
   return {
     setSizeByScreen,
     isSmallDevice,
@@ -9,65 +54,3 @@ export default function useDimensions() {
     isLargeDevice,
   };
 }
-
-export const isSmallDevice = () => {
-  const { innerWidth: width, innerHeight: height } = window;
-  // Determinar a classe de tamanho com base na largura e altura da tela em pixels
-  if (width <= 675) {
-    return true;
-  }
-};
-
-export const isMediumDevice = () => {
-  const { innerWidth: width, innerHeight: height } = window;
-  if (width > 675 && width < 1450) {
-    //411
-    return true;
-  }
-
-  return false;
-};
-
-export const isLargeDevice = () => {
-  const { innerWidth: width, innerHeight: height } = window;
-  if (width >= 1450) {
-    return true;
-  }
-  return false;
-};
-export const setSizeByScreen = (
-  smallScreen: number,
-  mediumScreen: number,
-  largeScreen: number
-) => {
-  const getWindowDimensions = () => {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height,
-    };
-  };
-  const [windowDimensions, setWindowDimensions] = useState<
-    { width: number; height: number } | undefined
-  >(undefined);
-
-  useEffect(() => {
-    setWindowDimensions(getWindowDimensions());
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  if (!windowDimensions) return;
-  if (windowDimensions.width <= 675) {
-    return smallScreen;
-  }
-  if (windowDimensions.width > 675 && windowDimensions.width < 1450) {
-    return mediumScreen;
-  }
-
-  return largeScreen;
-};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { ReactNode, useState } from "react";
 import Typewritter from "./components/typing";
 import useDimensions from "./hook/useDimensions";
 import "./styles.css";
@@ -7,41 +8,235 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
   const { setSizeByScreen, isLargeDevice } = useDimensions();
+  const [isFadingOut, setIsFadingOut] = useState(false);
+  const [content, setContent] = useState<ReactNode | undefined>(undefined);
+
+  const divineFall = () => {
+    return (
+      <div>
+        <h1 className="whitespace-nowrap" style={{ fontSize: "2rem" }}>
+          The Divine Fall
+        </h1>
+        <p className="text-justify" style={{ fontSize: "1rem" }}>
+          O desenvolvimento de The Divine Fall foi meu primeiro contato com
+          GameJams, tive a oportunidade de trabalhar ao lado de meus colegas da
+          DEV-U para desenvolver esse trabalho. O código, infelizmente está
+          perdido no limbo do tempo, mas continuo creditado sob o nome Schultz0.
+          Minha principal contribuição para o projeto foi no Level Design e
+          história.
+        </p>
+        <a>itch.io</a>
+      </div>
+    );
+  };
+
+  const saeg = (
+    <div>
+      <h1
+        className="whitespace-nowrap"
+        style={{ fontSize: setSizeByScreen(30, 50, 75) }}
+      >
+        New Content Here!
+      </h1>
+      <p
+        className="text-justify"
+        style={{ fontSize: setSizeByScreen(13, 15, 17) }}
+      >
+        This is the new content that appears after the fade-out animation.
+      </p>
+    </div>
+  );
+
+  const caragua = (
+    <div>
+      <h1
+        className="whitespace-nowrap"
+        style={{ fontSize: setSizeByScreen(30, 50, 75) }}
+      >
+        New Content Here!
+      </h1>
+      <p
+        className="text-justify"
+        style={{ fontSize: setSizeByScreen(13, 15, 17) }}
+      >
+        This is the new content that appears after the fade-out animation.
+      </p>
+    </div>
+  );
+
+  const ssad = (
+    <div>
+      <h1
+        className="whitespace-nowrap"
+        style={{ fontSize: setSizeByScreen(30, 50, 75) }}
+      >
+        New Content Here!
+      </h1>
+      <p
+        className="text-justify"
+        style={{ fontSize: setSizeByScreen(13, 15, 17) }}
+      >
+        This is the new content that appears after the fade-out animation.
+      </p>
+    </div>
+  );
+
+  const handleClick = (newContent: ReactNode | undefined) => {
+    setIsFadingOut(true);
+    setTimeout(() => {
+      setIsFadingOut(false);
+      setContent(newContent);
+    }, 1000);
+  };
 
   return (
     <main className="flex flex-col items-center justify-center">
       <section className="flex flex-col my-10 w-[90vw] h-[800px] pb-12 rounded-[50px] bg-[#222222]">
         <nav className="flex flex-row h-20 mt-10 w-full">
           <div className="ml-10 flex w-full justify-start">
-            <img src={logo.src} />
+            <img
+              style={{
+                width: setSizeByScreen(50, 60, 90),
+                height: setSizeByScreen(50, 60, 90),
+              }}
+              src={logo.src}
+            />
           </div>
           <div className="flex w-full justify-end gap-5 mr-10 text-[#CDCDCD]">
             <a>
-              <FaGithub size={setSizeByScreen(25, 50, 75)} />
+              <FaGithub size={setSizeByScreen(40, 45, 75)} />
             </a>
             <a>
-              <FaLinkedin size={setSizeByScreen(25, 50, 80)} />
+              <FaLinkedin size={setSizeByScreen(45, 50, 80)} />
             </a>
           </div>
         </nav>
-        <div className="flex flex-row w-full h-full mx-16">
+        <div className="flex flex-row w-full h-full">
           <div
-            style={{ paddingTop: setSizeByScreen(55, 60, 100) }}
-            className="flex flex-col w-full h-full"
+            style={{
+              paddingTop: setSizeByScreen(40, 50, 70),
+              marginLeft: setSizeByScreen(20, 30, 75),
+            }}
+            className={`flex flex-col w-[75%] h-full ${
+              isFadingOut ? "fade-out" : "fade-in"
+            }`}
           >
-            <h1 style={{ fontSize: setSizeByScreen(55, 50, 75) }}>
-              <Typewritter text={"Olá Mundo!"} delay={80} />
-            </h1>
-            <p style={{ fontSize: setSizeByScreen(15, 20, 20) }}>
-              Meu nome é Pedro Vidal, eu sou um desenvolvedor Front-End e Mobile
-              de São Paulo. Desenvolvi aplicações no ramo do serviço público que
-              se destacaram pela sua facilidade de uso e aprovação dos clientes.
-            </p>
+            {content ? (
+              content
+            ) : (
+              <>
+                <h1
+                  className="w-[10vw] whitespace-nowrap"
+                  style={{ fontSize: setSizeByScreen(30, 50, 75) }}
+                >
+                  <Typewritter text={"Olá Mundo!"} delay={80} />
+                </h1>
+                <p
+                  className="text-justify"
+                  style={{ fontSize: setSizeByScreen(13, 15, 17) }}
+                >
+                  Meu nome é Pedro Vidal, sou Desenvolvedor Front-End e Mobile
+                  de São Paulo. Desenvolvi aplicações no ramo do serviço público
+                  que se destacaram pela sua facilidade de uso e aprovação dos
+                  clientes.
+                </p>
+                &nbsp;
+                <div
+                  className="text-justify"
+                  style={{ fontSize: setSizeByScreen(13, 15, 17) }}
+                >
+                  <p>Fui Vice-Presidente do D.A IFSP-CAR em 2024.</p>
+                  &nbsp;
+                  <p>
+                    {" "}
+                    Meus hobbies pessoais incluem: Desenvolvimento de jogos e
+                    mods, muay thai, academia e resolução de problemas.
+                  </p>
+                </div>
+                <h1
+                  className="whitespace-nowrap pt-10"
+                  style={{ fontSize: setSizeByScreen(30, 50, 45) }}
+                >
+                  Afiliações:
+                </h1>
+                <div>
+                  <a
+                    target="_blank"
+                    href="https://asimovjr.com.br/"
+                    className="inline underline font-bold"
+                  >
+                    Asimov Jr.
+                  </a>{" "}
+                  | 2021 {"->"} 2022
+                </div>
+                <br />
+                <div>
+                  <a
+                    target="_blank"
+                    href="https://dev-unifei.github.io/"
+                    className="inline underline font-bold"
+                  >
+                    Dev-U
+                  </a>{" "}
+                  | 2022 {"->"} 2023
+                </div>
+                <br />
+                <div>
+                  <p className="whitespace-nowrap">
+                    <a
+                      target="_blank"
+                      href="https://puah.com.br/"
+                      className="inline underline font-bold"
+                    >
+                      Puah Consultoria
+                    </a>{" "}
+                    | 2023 {"->"} Atual
+                  </p>
+                </div>
+              </>
+            )}
           </div>
-          <div style={{ paddingLeft: setSizeByScreen(20, 100, 100), fontSize: setSizeByScreen(17, 17, 16) }} className="flex flex-col w-full h-full justify-center">
-            <a>156 Caraguá</a>
-            <a>SAEG</a>
-            <a>SSAD{isLargeDevice() ? " - Sistema de Solicitações e Acompanhamento de Demandas" : undefined}</a>
+          <div
+            style={{
+              paddingTop: setSizeByScreen(45, 50, 100),
+              marginRight: setSizeByScreen(20, 30, 75),
+              fontSize: setSizeByScreen(13, 15, 17),
+            }}
+            className="flex flex-col w-full h-full items-end"
+          >
+            <h1
+              className="whitespace-nowrap pb-10"
+              style={{ fontSize: setSizeByScreen(30, 50, 45) }}
+            >
+              Projetos
+            </h1>
+            <a
+              className="inline underline font-bold"
+              onClick={() => handleClick(caragua)}
+            >
+              156 Caraguá
+            </a>
+            <a
+              className="inline underline font-bold"
+              onClick={() => handleClick(saeg)}
+            >
+              SAEG
+            </a>
+            <a
+              className="inline underline font-bold"
+              onClick={() => handleClick(ssad)}
+            >
+              SSAD
+              {isLargeDevice()
+                ? " - Sistema de Solicitações e Acompanhamento de Demandas"
+                : undefined}
+            </a>
+            <a
+              className="inline underline font-bold"
+              onClick={() => handleClick(divineFall())}
+            >
+              The Divine Fall
+            </a>
           </div>
         </div>
       </section>
