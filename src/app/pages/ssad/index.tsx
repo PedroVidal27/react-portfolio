d7834b208a77e05285cea27553b175cacd6ce47b
PedroVidal27 <pedrovidal2703@gmail.com>
2024-07-02 21:58:09 -0300

    :bug: fix layout for ssad

--- src/app/pages/ssad/index.tsx
+++ src/app/pages/ssad/index.tsx
@@ -15,22 +15,12 @@ export default function SSAD() {
         className="text-justify"
         style={{ fontSize: setSizeByScreen(12, 13, 15, 17) }}
       >
-        O Sistema de Solicitações e Acompanhamento de Demandas, desenvolvido em
-        ASP.NET, gerencia os aplicativos 156 SAEG e 156 Caraguatatuba. Ele
-        integra o GLPI e oferece ferramentas para otimizar a gestão de demandas.
+        O Sistema de Solicitações e Acompanhamento de Demandas foi desenvolvido
+        em ASP.NET. Ele oferece ferramentas para otimizar a gestão de demandas.
         O teleatendente pode encaminhar e notificar as secretarias responsáveis,
         garantindo um atendimento ágil e eficiente. As funcionalidades do
         sistema asseguram uma administração eficaz e uma resposta rápida às
         solicitações.
-      </p>
-      &nbsp;
-      <p
-        className="text-justify"
-        style={{ fontSize: setSizeByScreen(12, 13, 15, 17) }}
-      >
-        No meu trabalho com o Sistema de Solicitações e Acompanhamento de
-        Demandas, fui responsável pela manutenção e pelo suporte na
-        implementação de novas funcionalidades.
       </p>
       &nbsp;
       <div style={{ fontSize: setSizeByScreen(19, 21, 23, 25) }}>
